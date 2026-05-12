cf9d9117a725d9ea8f9b1f9bc8910c51ed397abb
AkashAshokWego <akashashok@wegofin.com>
2026-05-12 16:01:59 +0530

    design

--- src/components/LocationSelector.tsx
+++ src/components/LocationSelector.tsx
@@ -73,63 +73,71 @@ const LocationSelector: React.FC = () => {
   }, [selectedCity, selectedState, selectedCountry])
 
   return (
-    <div className="location-selector-container">
-      <h1>Location Selector</h1>
-      <div className="dropdowns-wrapper">
-        <div className="dropdown-group">
-          <label htmlFor="country">Select Country</label>
-          <select
-            id="country"
-            value={selectedCountry}
-            onChange={(e) => setSelectedCountry(e.target.value)}
-            disabled={loading}
-          >
-            <option value="">-- Choose a Country --</option>
-            {countries.map((country) => (
-              <option key={country} value={country}>
-                {country}
-              </option>
-            ))}
-          </select>
+    <div className="location-selector-wrapper">
+      <div className="location-selector-container">
+        <h1>Select Location</h1>
+        
+        <div className="dropdowns-wrapper">
+          <div className="dropdown-group">
+            <select
+              id="country"
+              value={selectedCountry}
+              onChange={(e) => setSelectedCountry(e.target.value)}
+              disabled={loading}
+              className="dropdown-select"
+            >
+              <option value="">Select Country</option>
+              {countries.map((country) => (
+                <option key={country} value={country}>
+                  {country}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <div className="dropdown-group">
+            <select
+              id="state"
+              value={selectedState}
+              onChange={(e) => setSelectedState(e.target.value)}
+              disabled={!selectedCountry || loading}
+              className="dropdown-select"
+            >
+              <option value="">Select State</option>
+              {states.map((state) => (
+                <option key={state} value={state}>
+                  {state}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <div className="dropdown-group">
+            <select
+              id="city"
+              value={selectedCity}
+              onChange={(e) => setSelectedCity(e.target.value)}
+              disabled={!selectedState || loading}
+              className="dropdown-select"
+            >
+              <option value="">Select City</option>
+              {cities.map((city) => (
+                <option key={city} value={city}>
+                  {city}
+                </option>
+              ))}
+            </select>
+          </div>
         </div>
 
-        <div className="dropdown-group">
-          <label htmlFor="state">Select State</label>
-          <select
-            id="state"
-            value={selectedState}
-            onChange={(e) => setSelectedState(e.target.value)}
-            disabled={!selectedCountry || loading}
-          >
-            <option value="">-- Choose a State --</option>
-            {states.map((state) => (
-              <option key={state} value={state}>
-                {state}
-              </option>
-            ))}
-          </select>
-        </div>
-
-        <div className="dropdown-group">
-          <label htmlFor="city">Select City</label>
-          <select
-            id="city"
-            value={selectedCity}
-            onChange={(e) => setSelectedCity(e.target.value)}
-            disabled={!selectedState || loading}
-          >
-            <option value="">-- Choose a City --</option>
-            {cities.map((city) => (
-              <option key={city} value={city}>
-                {city}
-              </option>
-            ))}
-          </select>
-        </div>
+        {selection && (
+          <div className="selection-display">
+            <p>
+              You selected <strong>{selectedCity}</strong>, <span className="gray-text">{selectedState}, {selectedCountry}</span>
+            </p>
+          </div>
+        )}
       </div>
-
-      {selection && <div className="selection-display"><p>{selection}</p></div>}
-      {loading && <p className="loading">Loading...</p>}
     </div>
   )
 }
