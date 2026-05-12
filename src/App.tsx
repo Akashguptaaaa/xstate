cec978f12dd9d3cc75a88e7fc1fd36760eca78fd
AkashAshokWego <akashashok@wegofin.com>
2026-05-12 15:16:02 +0530

    xstate

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import LocationSelector from './components/LocationSelector'
+
+function App() {
+  return (
+    <>
+      <section id="center">
+        <LocationSelector />
+      </section>
+    </>
+  )
+}
+
+export default App
